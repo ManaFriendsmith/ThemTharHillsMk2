8d2eadd0de9b88211f0fd7534cdd62806b794729
ManaFriendsmith <42ah42@gmail.com>
2025-12-01 16:15:29 -0500

    no lithium in marzelvane recipe when moved to maraxsis

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,7 +2,7 @@ local misc = require("__pf-functions__/misc")
 local rm = require("__pf-functions__/recipe-manipulation")
 
 --do this in DFF so brimstuff gets its turn.
-if misc.difficulty == 3 and mods["space-age"] and #data.raw.recipe["cryogenic-plant"].ingredients > 6 then
+if misc.difficulty == 3 and mods["space-age"] and #data.raw.recipe["cryogenic-plant"].ingredients > 6 and not mods["maraxsis"] then
     rm.AddIngredient("hydrocoptic-marzelvane", "lithium-plate", 1)
     rm.RemoveIngredient("cryogenic-plant", "lithium-plate", 99999)
 end
