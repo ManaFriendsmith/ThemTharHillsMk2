94128419a319436c4c1b1c2d6cd270a19fa98746
ManaFriendsmith <42ah42@gmail.com>
2025-12-12 19:00:54 -0500

    recycler output resizing

--- data-updates.lua
+++ data-updates.lua
@@ -16,4 +16,17 @@ require("compat.deadlock")
 if mods["quality"] and misc.last_pf_mod == "ThemTharHillsMk2" then
     rm.FixStackingRecycling()
     require("__quality__/data-updates.lua")
+
+    local biggest_result_list = data.raw.furnace.recycler.result_inventory_size
+    for k, v in pairs(data.raw.recipe) do
+      if v.type == "recycling" or v.type == "recycling-or-hand-crafting" then
+        if v.results and #v.results > biggest_result_list then
+          biggest_result_list = #v.results
+        end
+      end
+    end
+    data.raw.furnace.recycler.result_inventory_size = biggest_result_list
+    if mods["Age-of-Production"] then
+        data.raw.furnace["aop-salvager"].result_inventory_size = biggest_result_list
+    end
 end
