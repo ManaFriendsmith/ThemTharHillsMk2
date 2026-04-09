97c6b1691a8a55cef7ef214992cccd6d2c105b16
ManaFriendsmith <42ah42@gmail.com>
2026-04-09 09:41:35 -0400

    sort scrap products by yield

--- data-updates.lua
+++ data-updates.lua
@@ -29,4 +29,8 @@ if mods["quality"] and misc.last_pf_mod == "ThemTharHillsMk2" then
     if mods["Age-of-Production"] then
         data.raw.furnace["aop-salvager"].result_inventory_size = biggest_result_list
     end
-end
+
+    rm.SortScrapProducts("scrap-recycling")
+    rm.SortScrapProducts("cerys-nuclear-scrap-recycling")
+    rm.SortScrapProducts("ancient-military-wreckage-recycling")
+ end
